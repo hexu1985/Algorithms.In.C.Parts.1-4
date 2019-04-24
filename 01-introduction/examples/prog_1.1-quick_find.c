8046d459cd5f0e24bebb62ae50ab1fe394a9d643
hexu <hexu_1985@sina.com>
2019-04-24 14:04:53 +0800

    update 01-introduction

--- 01-introduction/examples/prog_1.1-quick_find.c
+++ 01-introduction/examples/prog_1.1-quick_find.c
@@ -6,18 +6,18 @@
 
 int main(void)
 {
-  int p, q, i, t, id[N];
+	int p, q, i, t, id[N];
 
-  for (i = 0; i < N; i++)
-    id[i] = i;
-  while (scanf("%d %d\n", &p, &q) == 2)
-    {
-      if (id[p] == id[q])
-	continue;
-      for (t = id[p], i = 0; i < N; i++)
-	if (id[i] == t)
-	  id[i] = id[q];
-      printf(" %d %d\n", p, q);
-    }
-  return 0;
+	for (i = 0; i < N; i++)
+		id[i] = i;
+	while (scanf("%d %d\n", &p, &q) == 2)
+	{
+		if (id[p] == id[q])
+			continue;
+		for (t = id[p], i = 0; i < N; i++)
+			if (id[i] == t)
+				id[i] = id[q];
+		printf(" %d %d\n", p, q);
+	}
+	return 0;
 }
--- 01-introduction/examples/prog_1.2-quick-union.c
+++ 01-introduction/examples/prog_1.2-quick-union.c
@@ -6,18 +6,18 @@
 
 int main(void)
 {
-  int p, q, i, j, id[N];
+	int p, q, i, j, id[N];
 
-  for (i = 0; i < N; i++)
-    id[i] = i;
-  while (scanf("%d %d\n", &p, &q) == 2)
-    {
-      for (i = p; i != id[i]; i = id[i]);
-      for (j = q; j != id[j]; j = id[j]);
-      if (i == j)
-	continue;
-      id[i] = j;
-      printf(" %d %d\n", p, q);
-    }
-  return 0;
+	for (i = 0; i < N; i++)
+		id[i] = i;
+	while (scanf("%d %d\n", &p, &q) == 2)
+	{
+		for (i = p; i != id[i]; i = id[i]);
+		for (j = q; j != id[j]; j = id[j]);
+		if (i == j)
+			continue;
+		id[i] = j;
+		printf(" %d %d\n", p, q);
+	}
+	return 0;
 }

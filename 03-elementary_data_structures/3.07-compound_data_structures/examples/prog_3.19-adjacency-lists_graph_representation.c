36a2ae45c0f76ded0eee87ec8a7cc3df9c24b4dc
hexu <hexu@sensetime.com>
2019-05-29 18:31:00 +0800

    update 3.07

--- 03-elementary_data_structures/3.07-compound_data_structures/examples/prog_3.19-adjacency-lists_graph_representation.c
+++ 03-elementary_data_structures/3.07-compound_data_structures/examples/prog_3.19-adjacency-lists_graph_representation.c
@@ -6,44 +6,45 @@
 typedef struct node *link;
 struct node
 {
-  int v;
-  link next;
+	int v;
+	link next;
 };
 
 link new(int v, link next)
 {
-  link x = malloc(sizeof *x);
-  x->v = v;
-  x->next = next;
-  return x;
+	link x = malloc(sizeof *x);
+	x->v = v;
+	x->next = next;
+	return x;
 }
 
 void print_list(link t, int x)
 {
-  printf("%d - ", x);
-  while (t != NULL)
-    {
-      printf("%d ", t->v);
-      t = t->next;
-    }
-  printf("\n");
+	printf("%d - ", x);
+	while (t != NULL)
+	{
+		printf("%d ", t->v);
+		t = t->next;
+	}
+	printf("\n");
 }
 
 int main(void) 
 {
-  int i, j;
-  link adj[V];
+	int i, j;
+	link adj[V];
 
-  for (i = 0; i < V; i++)
-    adj[i] = NULL;
-  printf("Enter the edges: ");
-  while (scanf("%d %d\n", &i, &j) == 2)
-    {
-      adj[j] = new(i, adj[j]);
-      adj[i] = new(j, adj[i]);
-    }
-  for (i = 0; i < V; i++)
-    print_list(adj[i], i);
-  
-  return 0;
+	for (i = 0; i < V; i++)
+		adj[i] = NULL;
+	printf("Enter the edges: ");
+	while (scanf("%d %d\n", &i, &j) == 2)
+	{
+		adj[j] = new(i, adj[j]);
+		adj[i] = new(j, adj[i]);
+	}
+	printf("\n");
+	for (i = 0; i < V; i++)
+		print_list(adj[i], i);
+
+	return 0;
 }
--- 03-elementary_data_structures/3.07-compound_data_structures/examples/prog_3.20-two_dimensional-array-of-lists/main.c
+++ 03-elementary_data_structures/3.07-compound_data_structures/examples/prog_3.20-two_dimensional-array-of-lists/main.c
@@ -7,8 +7,8 @@
 typedef struct node *link;
 struct node
 {
-  point p;
-  link next;
+	point p;
+	link next;
 };
 link **grid;
 int G;
@@ -16,50 +16,50 @@ float d;
 int cnt = 0;
 void gridinsert(float x, float y)
 {
-  int i, j;
-  link s;
-  int X = x * G + 1;
-  int Y = y * G + 1;
-  link t = malloc(sizeof *t);
+	int i, j;
+	link s;
+	int X = x * G + 1;
+	int Y = y * G + 1;
+	link t = malloc(sizeof *t);
 
-  t->p.x = x;
-  t->p.y = y;
-  for (i = X-1; i <= X+1; j++)
-    for (s = grid[i][j]; s != NULL; s = s->next)
-      if (distance(s->p, t->p) < d)
-	cnt++;
-  t->next = grid [X][Y];
-  grid[X][Y] = t;
+	t->p.x = x;
+	t->p.y = y;
+	for (i = X-1; i <= X+1; i++)
+		for (j = Y-1; j <= Y+1; j++)
+			for (s = grid[i][j]; s != NULL; s = s->next)
+				if (distance(s->p, t->p) < d)
+					cnt++;
+	t->next = grid [X][Y];
+	grid[X][Y] = t;
 }
 
 float randFloat()
 {
-      float r = (float)rand()/(float)RAND_MAX;
-      return r;
+	float r = (float)rand()/(float)RAND_MAX;
+	return r;
 }
 
 link **malloc2d(int r, int c)
 {
-  int i;
-  link **t = malloc(r * sizeof(int*));
-  for (i = 0; i < r; i++)
-    t[i] = malloc(c * sizeof(int));
-  return t;
+	int i;
+	link **t = malloc(r * sizeof(link*));
+	for (i = 0; i < r; i++)
+		t[i] = malloc(c * sizeof(link));
+	return t;
 }
 
 int main(int argc, char *argv[])
 {
-  int i, j, N = atoi(argv[1]);
+	int i, j, N = atoi(argv[1]);
 
-  srand(time(NULL));
-  d = atof(argv[2]);
-  grid = malloc2d(G+2, i++);
-  for (i = 0; i < G+2; i++)
-    for (j = 0; j < G+2; j++)
-      grid[i][j] = NULL;
-  for (i = 0; i < N; i++)
-    gridinsert(randFloat(), randFloat());
-  printf("%d edges sorter than %f\n", cnt, d);
+	d = atof(argv[2]); G = 1/d;
+	grid = malloc2d(G+2, G+2);
+	for (i = 0; i < G+2; i++)
+		for (j = 0; j < G+2; j++)
+			grid[i][j] = NULL;
+	for (i = 0; i < N; i++)
+		gridinsert(randFloat(), randFloat());
+	printf("%d edges sorter than %f\n", cnt, d);
 
-  return 0;
+	return 0;
 }
